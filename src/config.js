0c703fdb2296b4efad071be0525c93754057cfc1
David Manzano <dmanzanod@gmail.com>
2022-10-03 15:43:35 -0300

    modificacion de parametros de la base de datos

--- src/config.js
+++ src/config.js
@@ -1,10 +1,15 @@
 import {config} from "dotenv";
 
 config();
-
+ 
 export default {
-    host: process.env.HOST || "",
-    database: process.env.DATABASE || "",
-    user: process.env.USER || "",
-    password: process.env.PASSWORD || "",
+//host: process.env.HOST || "localhost",
+    //database: process.env.DATABASE || "zazu_mobil",
+    //user: process.env.USER || "root",
+    //password: process.env.PASSWORD || "password",
+    host     : 'ec2-3-87-203-241.compute-1.amazonaws.com',
+    port     : '3306',
+    user     : 'zazudb2',
+    password : 'zazu2023',
+    database : 'zazu_mobil'
 };
