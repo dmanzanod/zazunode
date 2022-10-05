18622187473df838a3242dc294bbd3c316023154
David Manzano <dmanzanod@gmail.com>
2022-10-05 17:40:53 -0300

    variables actualizadas

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -5,7 +5,6 @@ const getCateoria = async(request, response) => {
     try{
         const connection= await getConnection();
         const result = await connection.query("SELECT categoria as name FROM `lubricentro_productos` group by CATEGORIA");
-        console.log(result);
         response.json(result);
     }catch(error){
         response.status(500);
@@ -20,7 +19,6 @@ const getProducto = async(request, response) => {
         const connection= await getConnection();
         let qString = "SELECT NOMBREPRODUCTO as name FROM lubricentro_productos WHERE NOMBREPRODUCTO LIKE CONCAT('%', ? , '%') GROUP BY NOMBREPRODUCTO"; 
         const result = await connection.query(qString, nombre);
-        console.log(result);
         response.json(result);
     }catch(error){
         response.status(500);
@@ -38,8 +36,6 @@ const getFormato = async(request, response) => {
             const connection= await getConnection();
             let qString = "SELECT CONCAT(envase_disponible, ' $', PRECIO_REGISTRADO) as name from lubricentro_productos where NOMBREPRODUCTO = ?"; 
             const result = await connection.query(qString, aux1);
-            console.log(result);
-            console.log(aux1);
             response.json(result);
         }catch(error){
             response.status(500);
@@ -55,9 +51,8 @@ const getFormato = async(request, response) => {
                 const aux = nombre.replace('{"name":"', '');
                 const aux1 = aux.replace('"}', '');
                 const connection= await getConnection();
-                let qString = "SELECT NOMBREPRODUCTO as name from lubricentro_productos where CATEGORIA = ?"; 
+                let qString = "SELECT NOMBREPRODUCTO as name from lubricentro_productos where CATEGORIA = ? GROUP BY NOMBREPRODUCTO"; 
                 const result = await connection.query(qString, aux1);
-                console.log(result);
                 response.json(result);
             }catch(error){
                 response.status(500);
